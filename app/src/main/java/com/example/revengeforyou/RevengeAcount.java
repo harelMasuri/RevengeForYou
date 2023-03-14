4dcff22b9c1c5758cf0a9a125ff87887c24daba1
chock666 <yehonatancf2006@gmail.com>
2023-03-14 17:27:26 +0200

    fix bottom nav bar

--- app/src/main/java/com/example/revengeforyou/RevengeAcount.java
+++ app/src/main/java/com/example/revengeforyou/RevengeAcount.java
@@ -21,7 +21,7 @@ public class RevengeAcount extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_revenge_acount);
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
 
